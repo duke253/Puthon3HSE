0ea3447c79a4644af16238b69bd2740f2105cd77
Grigoriy I <g.duke253@gmail.com>
2020-06-03 00:46:50 +0300

    #Whitespaces added

--- Test_2.py
+++ Test_2.py
@@ -4,8 +4,8 @@ pinguin2 = '  (o o)   '
 pinguin3 = ' /  V  \  '
 pinguin4 = '/(  _  )\ '
 pinguin5 = '  ^^ ^^   '
-print(pinguin1*n)
-print(pinguin2*n)
-print(pinguin3*n)
-print(pinguin4*n)
-print(pinguin5*n)
+print(pinguin1 * n)
+print(pinguin2 * n)
+print(pinguin3 * n)
+print(pinguin4 * n)
+print(pinguin5 * n)
--- Test_4.py
+++ Test_4.py
@@ -1,3 +1,3 @@
 n = int(input())
 k = int(input())
-print(k%n)
+print(k % n)
