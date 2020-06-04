afb4a0fed021ca31934d2a67b36634759a621a1c
Grigoriy I <g.duke253@gmail.com>
2020-06-04 11:51:01 +0300

    #files renamed

---
+++ penguins.py
@@ -0,0 +1,11 @@
+n = int(input())
+penguin1 = '   _~_    '
+penguin2 = '  (o o)   '
+penguin3 = ' /  V  \  '
+penguin4 = '/(  _  )\ '
+penguin5 = '  ^^ ^^   '
+print(penguin1 * n)
+print(penguin2 * n)
+print(penguin3 * n)
+print(penguin4 * n)
+print(penguin5 * n)
