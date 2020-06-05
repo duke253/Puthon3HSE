e186252a913649b55ee76b545b70f7023d2a8331
Grigoriy I <g.duke253@gmail.com>
2020-06-05 23:28:04 +0300

    #e-clock2 ready

--- solution.py
+++ solution.py
@@ -1,3 +1,7 @@
-v = int(input())
-t = int(input())
-print(v * t % 109)
+tsec = int(input())
+h = tsec // 3600 % 24
+m1 = tsec // 60 % 60 // 10
+m2 = tsec // 60 % 60 % 10
+s1 = tsec % 60 // 10
+s2 = tsec % 60 % 10
+print(h, ":", m1, m2, ":", s1, s2, sep="")
